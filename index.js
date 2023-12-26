ee7407fe1166953fb8d875e9910fb1b17d84694f
J4jatin <117545430+J4jatin@users.noreply.github.com>
2023-12-27 01:57:22 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'whwre' });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT);
