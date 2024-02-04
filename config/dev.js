ea5b3c62a1ee767900bff1eea3fd1cb507eee6e4
J4jatin <117545430+J4jatin@users.noreply.github.com>
2024-02-04 19:58:52 +0530

    endingggg-tak

--- config/dev.js
+++ config/dev.js
@@ -4,10 +4,9 @@ module.exports = {
   googleClientID: '521318507279-a9in48uar2vg4eevnffklhvq7j5nr23v.apps.googleusercontent.com',
   googleClientSecret: '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',
   mongoURI: 'mongodb+srv://bobbyshahgli:uWnaaY4nOUgXLQ11@cluster0.cqhgxrr.mongodb.net/emailydb?retryWrites=true&w=majority',
-
   cookieKey: 'jlkjfdklajfdajiejijijfdf',
   stripePublishableKey: 'pk_test_51OWYDFSDcHIZG3fljhwWJvD7qZ0QPHnZc3o4maJCvq9KKhgDldGktam1cXbMFsNPk4wOSGt4HedKCEgXSKD2JRUd00Pa08pcYY',
   stripeSecretKey: 'sk_test_51OWYDFSDcHIZG3flz0Xd2j7bGvxTfikwY5IjTCppGMVhQRbR8EUfTKyi8wpnMU3k7zGOapbKtOIErKYNe1kC9XY100mcUEG5Ho',
-  sendGridKey: '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',
+  sendGridKey: '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',
   redirectDomain: "http://localhost:3000"
 };
